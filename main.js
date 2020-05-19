3eb10cfc9c933970b82bc8e455141bac571a6ec9
Pixelfireblade <joshuamoses@outlook.com>
2020-05-20 09:20:22 +1000

    first

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.static('public'));
+
 
 app.get('/', function(req, res) { // GET REQUEST
     // let rdata = fs.readFileSync('timeline.json');
@@ -12,24 +14,24 @@ app.get('/', function(req, res) { // GET REQUEST
 });
 
 app.post('/', function(req,res) {
-    const newEvent = {
-        "scene": req.query.scene,
-        "act": req.query.act,
-        "title": req.query.title,
-        "quote": req.query.quote,
-        "desc": req.query.desc,
-        "by": req.query.by,
-        "themes": JSON.parse(req.query.themes),
-    }
-    console.log(newEvent)
-    let rdata = fs.readFileSync('timeline.json');
-    let data = JSON.parse(rdata);
-    data.push(newEvent);
+    // const newEvent = {
+    //     "scene": req.query.scene,
+    //     "act": req.query.act,
+    //     "title": req.query.title,
+    //     "quote": req.query.quote,
+    //     "desc": req.query.desc,
+    //     "by": req.query.by,
+    //     "themes": JSON.parse(req.query.themes),
+    // }
+    // console.log(newEvent)
+    // let rdata = fs.readFileSync('./timeline.json');
+    // let data = JSON.parse(rdata);
+    // data.push(newEvent);
 
 
-    fs.writeFileSync('timeline.json', JSON.stringify(data));
+    // fs.writeFileSync('timeline.json', JSON.stringify(data));
 
-    res.send(data);
+    res.send("HEELLLO");
 });
 
 
