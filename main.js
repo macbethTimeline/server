a5aab8d8d661140815320ced20361664ce2d3150
Pixelfireblade <joshuamoses@outlook.com>
2020-05-20 10:38:10 +1000

    first

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(cors({
-    origin: 'https://macbethtimeline.github.io/contribute.html'
+    origin: 'https://macbethtimeline.github.io/'
   }));
 app.listen(process.env.PORT || 8080, () => console.log("Port used: "));
 
