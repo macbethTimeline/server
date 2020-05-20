8b79777ad71b8ae21a7ef71f00110bfe1bcd6dba
Pixelfireblade <joshuamoses@outlook.com>
2020-05-20 11:46:13 +1000

    first

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) { // GET REQUEST
 });
 
 app.post('/', function(req,res) {
-    console.log(req.body);
+    console.log(req.data);
     let thing = req.data
     const newEvent = {
         "scene": thing.scene,
